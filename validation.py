c71c3eb048df6a1b234939748c9b50502445d3ed
Albert Jiang <albert594250@gmail.com>
2017-02-23 21:07:56 +0000

    fldksjflasd

--- validation.py
+++ validation.py
@@ -7,10 +7,11 @@ import numpy as np
 
 def validate(cache_storage, video_size, new_storing_command):
 	data_config, video_config, endpoints, endpoint_connections, endpoint_datacenter_latency, requests = load_data("me_at_the_zoo.in")
-	used_space = np.sum(cache_storage[new_storing_command[1]])
+	stored = [ video_size[video] for video in cache_storage[new_storing_command[1]]]
+	used_space = np.sum(stored)
 	capacity = data_config["cache_size"]
 	print(video_size[new_storing_command[0]], used_space, capacity)
-	if (video_size[new_storing_command[0]] + used_space > capacity):
+	if (video_size[new_storing_command[0]] + used_space) > capacity:
 		print("f")
 		return False
 	print("t")
