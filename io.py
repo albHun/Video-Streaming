222d4f99dacf64a325d4ea0e79a09b27f73427c3
einsteinly <lieinsteinxi@gmail.com>
2017-02-23 19:33:39 +0000

    updated doc string in io

--- io.py
+++ io.py
@@ -2,15 +2,13 @@
 
 
 def load_data(filename):
+
+
     """
         Data returned: data_config, video_config, endpoints, requests
 
-        each endpoint is list of tuples: (cache_id, latency) [
-            (0, 1000),
-            (1, 500),
-            ...,
-            (n, 5000) n element, also the last element, is the data centre
-        ]
+        each endpoint is dictionary with the cache_id being the key and the latency being the value, 
+        the latency datacentre is stored in a key "datacentre"
 
         each request is a list of tuples: (video_id, endpoint_id, number of requests for the video)
     """
@@ -42,7 +40,7 @@ def load_data(filename):
     raw_data[0:1] = []
 
     endpoints = list()
-    endpoint = list()
+    endpoint = dict()
     cache_connections = 0
     datacentre_latency = 0
     index = 0
@@ -51,13 +49,15 @@ def load_data(filename):
     while endpoint_count < data_config["endpoints"]:
         #end point config line 
         line0 = raw_data[index]
-        datacentre_latency = line0[0]
+        endpoint["datacentre"] = line0[0]
         cache_connections = line0[1]
 
         #for each cache config
         for j in range(index, index + cache_connections + 1):
-            line = raw_data[j]
-            endpoints.append( (line[0], line[1]) )
+            line = raw_data[j] # line = {cache_id} {latency}
+            endpoint[line[0]] = line[1]
+        
+        endpoints.append(endpoint)
         index += cache_connections
         endpoint_count +=1
     
@@ -70,3 +70,6 @@ def load_data(filename):
 
     return data_config, video_config, endpoints, requests
 
+
+def output_data(caches):
+    
